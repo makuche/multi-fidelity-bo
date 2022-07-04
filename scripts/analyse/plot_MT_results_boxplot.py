4aca5e3d49fb819553255f0a931eb94aa7575e8c
Manuel Kuchelmeister <manuel.kuchelmeister@web.de>
2022-07-04 15:20:46 +0300

    Finished boxplot scripts for MT and TL experiments

--- scripts/analyse/plot_MT_results_boxplot.py
+++ scripts/analyse/plot_MT_results_boxplot.py
@@ -23,7 +23,14 @@ CONFIG = load_yaml(THESIS_DIR / 'scripts', '/config_mt.yaml')
               help='Chose between 2D or 4D.')
 @click.option('--tolerance', default=0.23, type=float,
               help='Tolerance level to plot convergence for.')
-def main(show_plots, dimension, tolerance):
+@click.option('--highest_fidelity', default='uhf', type=str,
+              help="Chose between 'uhf' and 'hf'.")
+@click.option('--print_non_converged', default=False, is_flag=True,
+              help='Print a sub-dataframe of unconverged runs')
+@click.option('--print_summary', default=False, is_flag=True,
+              help='Print summary statistics of the dataframe to the terminal')
+def main(show_plots, dimension, tolerance, highest_fidelity,
+         print_non_converged, print_summary):
     tolerances = np.array(CONFIG['tolerances'])
     if tolerance not in tolerances:
         raise Exception(f"Invalid tolerance level, chose from {tolerances}")
@@ -33,57 +40,90 @@ def main(show_plots, dimension, tolerance):
     tl_experiments = [THESIS_DIR / 'data/multi_task_learning' / 'processed' /
                       exp for exp in config.keys()]
 
+    # Don't load baseline experiments multiple times
+    bl_experiment_keys = list(set([config[exp][0] for exp in config]))
     bl_experiments = [THESIS_DIR / 'data/multi_task_learning' / 'processed' /
-                      config[exp][0] for exp in config]
+                      exp for exp in bl_experiment_keys]
     tl_exp_data = load_experiments(tl_experiments)
     bl_exp_data = load_experiments(bl_experiments)
     df = load_statistics_to_dataframe(bl_exp_data, tl_exp_data, num_exp=5)
 #    df.to_csv('mt_test.csv')
-    plot_convergence_as_boxplot(df, tolerance, dimension, show_plots)
+    plot_convergence_as_boxplot(
+        df, tolerance, dimension, highest_fidelity, show_plots,
+        print_non_converged, print_summary)
 
 
-def plot_convergence_as_boxplot(df, tolerance, dimension, show_plots):
+def plot_convergence_as_boxplot(
+        df, tolerance, dimension, highest_fidelity, show_plots,
+        print_not_converged, print_summary):
     tolerance_idx = np.argwhere(
         tolerance == np.array(np.array(CONFIG['tolerances']))).squeeze()
+
     plot_df = df[['name', 'iterations_to_gmp_convergence',
-                  'totaltime_to_gmp_convergence']]
+                  'totaltime_to_gmp_convergence',
+                  'highest_fidelity_iterations_to_gmp_convergence']]
+
     plot_df['iterations'] = plot_df[
         'iterations_to_gmp_convergence'].map(lambda x: x[tolerance_idx])
-    def scale_time(x, tolerance_idx=tolerance_idx):
-        if x[tolerance_idx] is None:
-            return None
-        else:
-            return x[tolerance_idx] / 3600
+    plot_df['Highest fidelity\niterations'] = plot_df[
+        'highest_fidelity_iterations_to_gmp_convergence'].map(
+            lambda x: x[tolerance_idx])
+    plot_df
     plot_df['CPU time [h]'] = plot_df[
-        'totaltime_to_gmp_convergence'].map(lambda x: scale_time(x))
-    # plot_df['totaltime'] = plot_df[
-    #     'totaltime_to_gmp_convergence'].map(lambda x: x[tolerance_idx]/3600)
+        'totaltime_to_gmp_convergence'].map(
+            lambda x: chose_value_with_tolerance(x, tolerance_idx))
 
-    fig = plt.figure(figsize=(6.5, 5))
-    plot_df_uhf = plot_df[plot_df['name'].str.contains('UHF') == False]
-    #plot_df_uhf = plot_df[plot_df['name'].str.contains('UHF') == True]
-    import IPython; IPython.embed()
-    exit()
+    if highest_fidelity == 'uhf':
+        plot_df = plot_df[plot_df['name'].str.contains('UHF') == True]
+    elif highest_fidelity == 'hf':
+        plot_df = plot_df[plot_df['name'].str.contains('UHF') == False]
+    else:
+        raise Exception("Invalid highest fidelity")
+
+    fig, axs = plt.subplots(2, 1, figsize=(6.5, 5.5))
     conditions_strategy = [
-        (plot_df_uhf['name'].str.contains('basic') ),
-        (plot_df_uhf['name'].str.contains('ICM1')),
-        (plot_df_uhf['name'].str.contains('ICM2'))]
-    strategies = ['Baseline', 'LF support', 'HF support']
+        (plot_df['name'].str.contains('basic') ),
+        (plot_df['name'].str.contains('ICM1')),
+        (plot_df['name'].str.contains('ICM2'))]
+    strategies = ['Baseline', r'LF $\rightarrow$ UHF',
+                  r'HF $\rightarrow$ UHF']
     conditions_approach = [
-        (plot_df_uhf['name'].str.contains('_r')),
-        (plot_df_uhf['name'].str.contains('ELCB1')),
-        (plot_df_uhf['name'].str.contains('ELCB3')),
-        (plot_df_uhf['name'].str.contains('ELCB6'))]
-    approaches = ['Single fidelity', 'Approach 1', 'Approach 3', 'Approach 6']
-    plot_df_uhf['Setup'] = np.select(conditions_strategy, strategies)
-    plot_df_uhf['Strategy'] = np.select(conditions_approach, approaches)
-    #import IPython; IPython.embed()
-    sns.boxplot(x='Setup', y='iterations', hue='Strategy',
-                data=plot_df_uhf, palette="Set2")
+        (plot_df['name'].str.contains('basic')),
+        (plot_df['name'].str.contains('ELCB1')),
+        (plot_df['name'].str.contains('ELCB3')),
+        (plot_df['name'].str.contains('ELCB6'))]
+    approaches = ['Single fidelity', 'MFBO approach 1',
+                  'MFBO approach 3',
+                  'MFBO approach 6']
+    if print_not_converged:
+        iterations_df = plot_df[['name', 'iterations']]
+        print(iterations_df[iterations_df['iterations'].isna()])
+    plot_df['Setup'] = np.select(conditions_strategy, strategies)
+    plot_df['Strategy'] = np.select(conditions_approach, approaches)
+    if print_summary:
+        print(plot_df.groupby(['Setup', 'Strategy'])\
+            ['CPU time [h]'].describe(percentiles=[.5]).round(2))
+    sns.boxplot(x='Setup', y='Highest fidelity\niterations', hue='Strategy',
+                whis=[0.25, 0.75], data=plot_df, palette="tab10", ax=axs[0])
+    sns.boxplot(x='Setup', y='CPU time [h]', hue='Strategy', whis=[0.25, 0.75],
+                data=plot_df, palette="tab10", ax=axs[1])
+    fig.suptitle(
+        f'{dimension} Multi-task learning convergence results', fontsize=16)
+    fig.tight_layout()
+
     if show_plots:
         plt.show()
     else:
-        plt.savefig(FIGS_DIR / f'MT_convergence_boxplot_{dimension}_hf.pdf')
+        name = f'MT_convergence_boxplot_{dimension}_{highest_fidelity}'
+        plt.savefig(FIGS_DIR / f'{name}.pdf')
+
+
+def chose_value_with_tolerance(x, tolerance_idx, time_in_h=True):
+    if x[tolerance_idx] is None:
+        return None
+    else:
+        x_tol = x[tolerance_idx] / 3600 if time_in_h else x[tolerance_idx]
+        return x_tol
 
 if __name__ == '__main__':
     main()
--- scripts/analyse/plot_TL_results_boxplot.py
+++ scripts/analyse/plot_TL_results_boxplot.py
@@ -16,12 +16,25 @@ FIGS_DIR = THESIS_DIR / 'results/figs'
 CONFIG = load_yaml(THESIS_DIR / 'scripts', '/config_tl.yaml')
 
 
+ax_labels = [['a)', 'b)'], ['c)', 'd)']]
 sub_dataframes_2D = [('2HFbasic1', '2HFICM1'),
-                     ('2UHFbasic1_r', '2UHFICM1'),
-                     ('2UHFbasic1_r', '2UHFICM2')]
+                     ('2UHFbasic1_r', '2UHFICM1', '2UHFICM2')]
 sub_dataframes_4D = [('4HFbasic1', '4HFICM1'),
-                     ('4UHFbasic1_r', '4UHFICM1_r', '4UHFICM3_r'),
-                     ('4UHFbasic1_r', '4UHFICM2_r', '4UHFICM4_r')]
+                     ('4UHFbasic1_r', '4UHFICM1_r', '4UHFICM2_r', '4UHFICM3_r', '4UHFICM4_r')]
+convert_strings = {
+    '2HFbasic1': 'HF Baseline',
+    '2UHFbasic1_r': 'UHF Baseline',
+    '2HFICM1': r'LF $\rightarrow$ HF',
+    '2UHFICM1': r'LF $\rightarrow$ UHF',
+    '2UHFICM2': r'HF $\rightarrow$ UHF',
+    '4HFbasic1': 'HF Baseline',
+    '4UHFbasic1_r': 'UHF Baseline',
+    '4HFICM1': r'LF $\rightarrow$ HF',
+    '4UHFICM1_r': r'LF $\rightarrow$ UHF',
+    '4UHFICM3_r': r'LF $\rightarrow$ UHF',
+    '4UHFICM2_r': r'HF $\rightarrow$ UHF',
+    '4UHFICM4_r': r'HF $\rightarrow$ UHF'
+}
 titles = [r'LF $\rightarrow$ HF', r'LF $\rightarrow$ UHF',
           r'HF $\rightarrow$ UHF']
 
@@ -33,7 +46,9 @@ titles = [r'LF $\rightarrow$ HF', r'LF $\rightarrow$ UHF',
               help='Chose between 2D or 4D.')
 @click.option('--tolerance', default=0.23, type=float,
               help='Tolerance level to plot convergence for.')
-def main(show_plots, dimension, tolerance):
+@click.option('--print_summary', default=False, is_flag=True,
+              help='Print summary statistics of the dataframe to the terminal')
+def main(show_plots, dimension, tolerance, print_summary):
     tolerances = np.array(CONFIG['tolerances'])
     if tolerance not in tolerances:
         raise Exception(f"Invalid tolerance level, chose from {tolerances}")
@@ -45,80 +60,99 @@ def main(show_plots, dimension, tolerance):
     tl_exp_data = load_experiments(tl_experiments)
     bl_exp_data = load_experiments(bl_experiments)
     df = load_statistics_to_dataframe(bl_exp_data, tl_exp_data, num_exp=5)
-    df.to_csv('test.csv')
-    plot_convergence_as_boxplot(df, tolerance, dimension, show_plots)
+    plot_convergence_as_boxplot(
+        df, tolerance, dimension, show_plots, print_statistics)
 
 
-def plot_convergence_as_boxplot(df, tolerance, dimension, show_plots):
-    tolerance_idx = np.argwhere(
-        tolerance == np.array(np.array(CONFIG['tolerances']))).squeeze()
-    plot_df = df[['name', 'iterations_to_gmp_convergence',
-                  'totaltime_to_gmp_convergence', 'initpts']]
-    plot_df['iterations'] = plot_df[
-        'iterations_to_gmp_convergence'].map(lambda x: x[tolerance_idx])
-    plot_df['totaltime'] = plot_df[
-        'totaltime_to_gmp_convergence'].map(lambda x: x[tolerance_idx]/3600)
-    plot_df['tl_initpts'] = plot_df[
-        'initpts'].map(lambda x: x[1])
-    fig, axs = plt.subplots(2, 3, figsize=(6.5, 5), sharex=True)
-    sub_dataframes = sub_dataframes_2D if dimension == '2D' \
-        else sub_dataframes_4D
-    for ax_idx, exp_names in enumerate(sub_dataframes):
+def plot_convergence_as_boxplot(df, tolerance, dimension, show_plots,
+                                print_summary):
+
+    plot_df = create_plot_dataframe(df, tolerance)
+    dataframes = sub_dataframes_2D if dimension == '2D' else sub_dataframes_4D
+    fig, axs = plt.subplot_mosaic([['a)', 'c)', 'c)'], ['b)', 'd)', 'd)']],
+                                   figsize=(6.5, 6.5), constrained_layout=True)
+    for label, ax in axs.items():
+        # label physical distance in and down:
+        trans = mtransforms.ScaledTranslation(
+            10/72, -5/72, fig.dpi_scale_trans)
+        ax.text(0.0, 1.0, label, transform=ax.transAxes + trans,
+                fontsize=12, verticalalignment='top',
+                )
+    for ax_label, exp_names in zip(ax_labels, dataframes):
         names = [name for name in exp_names]
-        if len(names) == 3:
-            experiment_df = plot_df[
+        if len(names) == 5:
+            experiment_df = plot_df.loc[
+                (plot_df['name'] == names[0]) | (plot_df['name'] == names[1]) |
+                (plot_df['name'] == names[2]) | (plot_df['name'] == names[3]) |
+                (plot_df['name'] == names[4])]
+        elif len(names) == 3:
+            experiment_df = plot_df.loc[
                 (plot_df['name'] == names[0]) | (plot_df['name'] == names[1]) |
                 (plot_df['name'] == names[2])]
         else:
-            experiment_df = plot_df[
+            experiment_df = plot_df.loc[
                 (plot_df['name'] == names[0]) | (plot_df['name'] == names[1])]
-        sns.boxplot(x='tl_initpts', y='iterations', data=experiment_df,
-                    ax=axs[0, ax_idx], width=0.75, palette="Set2")
-        sns.boxplot(x='tl_initpts', y='totaltime', data=experiment_df,
-                    ax=axs[1, ax_idx], width=0.75, palette="Set2")
-        # sns.stripplot(x='tl_initpts', y='iterations', data=experiment_df,
-        #             ax=axs[0, ax_idx], dodge=True, marker='o', size=4,
-        #             color='black', jitter=True, alpha=.5)
-        # sns.stripplot(x='tl_initpts', y='totaltime', data=experiment_df,
-        #             ax=axs[1, ax_idx], dodge=True, marker='o', size=4,
-        #             color='black', jitter=True, alpha=.5)
-    trans = mtransforms.ScaledTranslation(10/72, -5/72, fig.dpi_scale_trans)
-    for idx, labels in enumerate([('a)', 'b)'), ('c)', 'd)'), ('e)', 'f)')]):
-        axs[0, idx].text(0.0, 1.0, labels[0],
-                         transform=axs[0, idx].transAxes + trans, fontsize=12,
-                         verticalalignment='top')
-        axs[1, idx].text(0.0, 1.0, labels[1],
-                         transform=axs[1, idx].transAxes + trans, fontsize=12,
-                         verticalalignment='top')
-        axs[0, idx].set_xlabel('')
-        axs[0, idx].set_ylabel('')
-        axs[1, idx].set_xlabel('Number of lower \nfidelity samples')
-        axs[1, idx].set_ylabel('')
-        if dimension == '2D':
-            axs[0, idx].set_ylim(0, 35)
-            axs[0, idx].set_title(titles[idx])
-        else:
-            axs[0, idx].set_ylim(0, 200)
-            axs[0, idx].set_title(titles[idx])
-    if dimension == '2D':
-        axs[1, 0].set_ylim(0, 0.35)
-        axs[1, 1].set_ylim(0, 120)
-        axs[1, 2].set_ylim(0, 120)
-    elif dimension == '4D':
-        axs[0, 1].set_ylim(0, 130)
-        axs[0, 2].set_ylim(0, 130)
-        axs[1, 0].set_ylim(0, 2.5)
-        axs[1, 1].set_ylim(0, 390)
-        axs[1, 2].set_ylim(0, 390)
-    axs[0, 0].set_ylabel('BO Iterations')
-    axs[1, 0].set_ylabel('CPU time [h]')
+        sns.boxplot(x='Setup', y='Highest fidelity iterations',
+                    data=experiment_df, ax=axs[ax_label[0]], width=0.75,
+                    palette="tab10", hue='Lower fid. samples',
+                    whis=[0.25, 0.75])
+        sns.boxplot(x='Setup', y='CPU time [h]', data=experiment_df,
+                    ax=axs[ax_label[1]], width=0.75, palette="tab10",
+                    hue='Lower fid. samples', whis=[0.25, 0.75])
+        if print_summary:
+            print(experiment_df.groupby(['Setup', 'Lower fid. samples'])\
+                ['CPU time [h]'].describe(percentiles=[.5]).round(2))
+    axs['a)'].set_title('HF accuracy')
+    axs['c)'].set_title('UHF accuracy')
+    for ax in ['a)', 'c)']:
+        axs[ax].set_xlabel('')
+        axs[ax].set_xticks([])
+        axs[ax].legend(loc='upper right', title='Lower fid.\nsamples')
+    axs['b)'].get_legend().remove()
+    axs['c)'].set_ylabel('')
+    axs['d)'].set_ylabel('')
+    axs['d)'].get_legend().remove()
     fig.suptitle(
-        f'{dimension} Transfer Learning convergence results', fontsize=16)
-    fig.tight_layout()
+        f'{dimension} Transfer learning convergence results', fontsize=16)
     if show_plots:
         plt.show()
     else:
         plt.savefig(f'results/figs/transfer_learning_boxplots_{dimension}.pdf')
+
+
+def create_plot_dataframe(df, tolerance):
+    tolerance_idx = np.argwhere(
+        tolerance == np.array(np.array(CONFIG['tolerances']))).squeeze()
+    plot_df = df[['name', 'iterations_to_gmp_convergence',
+                  'totaltime_to_gmp_convergence', 'initpts',
+                  'cumulative_num_highest_fidelity_samples']]
+    plot_df['iterations'] = plot_df[
+        'iterations_to_gmp_convergence'].map(
+            lambda x: chose_value_with_tolerance(x, tolerance_idx))
+    plot_df['totaltime'] = plot_df[
+        'totaltime_to_gmp_convergence'].map(
+            lambda x: chose_value_with_tolerance(x, tolerance_idx,
+                                                 time_in_h=True))
+    plot_df['tl_initpts'] = plot_df[
+        'initpts'].map(lambda x: x[1])
+    plot_df.drop_duplicates(
+        subset=['totaltime', 'tl_initpts', 'name'], inplace=True)
+    plot_df['setup'] = plot_df['name'].map(lambda x: convert_strings[x])
+    plot_df.rename(
+        columns={'tl_initpts': 'Lower fid. samples',
+                 'totaltime': 'CPU time [h]',
+                 'setup': 'Setup',
+                 'iterations': 'Highest fidelity iterations'},
+        inplace=True)
+    return plot_df
+
+
+def chose_value_with_tolerance(x, tolerance_idx, time_in_h=False):
+    if x[tolerance_idx] is None:
+        return None
+    else:
+        x_tol = x[tolerance_idx] / 3600 if time_in_h else x[tolerance_idx]
+        return x_tol
 
 
 if __name__ == '__main__':
